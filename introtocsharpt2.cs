d88bff3da24adbd329307b3e2d589056d04ae48e
programmerbender <165676852+programmerbender@users.noreply.github.com>
2024-04-02 22:49:31 +0800

    Add files via upload

---
+++ introtocsharpt2.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("Hello Goodbye");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Hello");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Goodbye");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
